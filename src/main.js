952011e5b32fca17d1230a07781dbe97e4b8cc39
Singulatarian <asingulatarian@gmail.com>
2023-12-23 15:05:25 -0800

    #0 Very minor style changes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import {createPinia} from 'pinia'
 import App from './App.vue'
 
+// Clear any background.js messages, since they persist across reloads
+console.clear()
+
 const params = new URLSearchParams(window.location.search)
 const app = createApp(App)
 app.use(createPinia())
